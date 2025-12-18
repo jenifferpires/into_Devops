d750fb6d3acefd1c9370bc0c8084f897e436402a
Jeniffer Pires <jenyfferhellen@gmail.com>
2025-12-18 00:20:18 -0300

    Script monitor.sh finalizado com cores e métricas de memória

--- scripts/monitor.sh
+++ scripts/monitor.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
-echo "--- RELATÓRIO DE SISTEMA ---"
+
+# Definindo cores
+VERDE='\033[0;32m'
+AZUL='\033[0;34m'
+SEM_COR='\033[0m'
+
+echo -e "${AZUL}--- RELATÓRIO DE SISTEMA ---${SEM_COR}"
 date
-echo "Usuário atual: $USER"
-echo "--- ESPAÇO EM DISCO ---"
+echo -e "Usuário atual: ${VERDE}$USER${SEM_COR}"
+
+echo -e "${AZUL}--- ESPAÇO EM DISCO ---${SEM_COR}"
 df -h | head -n 2
+
+echo -e "${AZUL}--- USO DE MEMÓRIA (MB) ---${SEM_COR}"
+free -m
